1ead2c8dbd4b188dd0d9dc574629f0185d247691
resqiar <resqiageng@gmail.com>
2023-11-21 15:24:34 +0700

    optimize some stuff

--- views/static/index.js
+++ views/static/index.js
@@ -1,20 +1,25 @@
 const container = document.getElementById("render");
+const decryptContainer = document.getElementById("render-decrypt");
 let morse = "";
 
 let pressedTime;
 
-window.addEventListener("keydown", function(e) {
-    if (pressedTime && (performance.now() - pressedTime) > 800) {
+window.addEventListener("keydown", function() {
+    if (pressedTime && (performance.now() - pressedTime) > 400) {
         renderSpace();
     }
+});
 
+window.addEventListener("keyup", function(e) {
     if (e.code === "Delete") {
         renderShort();
         pressedTime = performance.now();
-    } else if (e.shiftKey) {
+    } else if (e.key === "Shift") {
         renderLong();
         pressedTime = performance.now();
     }
+
+    decrypt();
 })
 
 function renderShort() {
@@ -30,4 +35,56 @@ function renderLong() {
 function renderSpace() {
     morse = morse + " ";
     container.innerHTML = morse;
+}
+
+const mtoa = new Map([
+    [".-", "A"],
+    ["-...", "B"],
+    ["-.-.", "C"],
+    ["-..", "D"],
+    [".", "E"],
+    ["..-.", "F"],
+    ["--.", "G"],
+    ["....", "H"],
+    ["..", "I"],
+    [".---", "J"],
+    ["-.-", "K"],
+    [".-..", "L"],
+    ["--", "M"],
+    ["-.", "N"],
+    ["---", "O"],
+    [".--.", "P"],
+    ["--.-", "Q"],
+    [".-.", "R"],
+    ["...", "S"],
+    ["-", "T"],
+    ["..-", "U"],
+    ["...-", "V"],
+    [".--", "W"],
+    ["-..-", "X"],
+    ["-.--", "Y"],
+    ["--..", "Z"],
+    ["-----", "0"],
+    [".----", "1"],
+    ["..---", "2"],
+    ["...--", "3"],
+    ["....-", "4"],
+    [".....", "5"],
+    ["-....", "6"],
+    ["--...", "7"],
+    ["---..", "8"],
+    ["----.", "9"],
+    [".-.-.", "+"],
+    ["-...-", "="],
+]);
+
+function decrypt() {
+    let result = "";
+    const res = morse.split(" ");
+
+    for (let i = 0; i < res.length; i++) {
+        if (mtoa.has(res[i])) result = result + mtoa.get(res[i]);
+    }
+
+    decryptContainer.innerText = result;
 }
