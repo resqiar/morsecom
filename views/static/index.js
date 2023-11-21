1c0aec2ffb8d3d18b5880bb1ef035487d9987716
resqiar <resqiageng@gmail.com>
2023-11-21 13:31:18 +0700

    change to dedicated keys

--- views/static/index.js
+++ views/static/index.js
@@ -1,48 +1,33 @@
 const container = document.getElementById("render");
 let morse = "";
 
-let startTime;
-let endTime;
-let spaceStartTime;
-let spaceEndTime;
+let pressedTime;
 
 window.addEventListener("keydown", function(e) {
-    if (e.code === `Delete`) {
-        startTime = performance.now();
-
-        if (spaceStartTime) {
-            spaceEndTime = performance.now();
-        }
-    }
-})
-
-window.addEventListener("keyup", function(e) {
-    if (e.code === `Delete`) {
+    if (pressedTime && (performance.now() - pressedTime) > 800) {
         renderSpace();
-        endTime = performance.now();
-        spaceStartTime = performance.now();
-        renderResult();
+    }
+
+    if (e.code === "Delete") {
+        renderShort();
+        pressedTime = performance.now();
+    } else if (e.shiftKey) {
+        renderLong();
+        pressedTime = performance.now();
     }
 })
 
-function renderResult() {
-    const time = endTime - startTime;
+function renderShort() {
+    morse = morse + ".";
+    container.innerHTML = morse;
+}
 
-    if (time < 100) {
-        morse = morse + ".";
-    } else {
-        morse = morse + "-";
-    }
-
+function renderLong() {
+    morse = morse + "-";
     container.innerHTML = morse;
 }
 
 function renderSpace() {
-    const time = spaceEndTime - spaceStartTime;
-
-    if (time > 800) {
-        morse = morse + " ";
-    }
-
+    morse = morse + " ";
     container.innerHTML = morse;
 }
